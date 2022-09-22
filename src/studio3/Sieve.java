423a2fc46bfee9b1d674137a2488857532aa62d7
miafo <miafo@LAPTOP-8TH479DR.dhcp.wustl.edu>
2022-09-22 14:08:29 -0500

    Arrays

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,39 @@
+
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		
+		
+		System.out.println("Pick a value of N:");
+		int n = in.nextInt();
+		
+		boolean [] numbers = new boolean[n+1];
+		
+		for (int i = 2; i < Math.sqrt(n); i++) {
+			int counter = 1;
+			
+			if(numbers[i] == false) {
+				for (int j = i*i; j<=n; j = j+i ) {
+					counter++;
+					
+					numbers[j] = true;
+					
+				}
+			}
+		}
+		
+		for (int i = 0; i<n; i++) {
+			if (numbers[i] == false) {
+				System.out.println(i);
+			}
+		}
+	}
+	
+}
